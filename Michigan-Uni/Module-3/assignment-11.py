49d9dd9cc5d9850ce0acb53c3885060456201bad
Giulia Ciprandi <giulia.ciprandi@gmail.com>
2020-07-12 20:53:26 -0300

    Create assignment-11.py

---
+++ Michigan-Uni/Module-3/assignment-11.py
@@ -0,0 +1,16 @@
+##FINDS ALL NUMBERS IN FILE
+##AND SUMS THEM
+
+import re
+
+file = "regex_sum_42.txt"
+
+with open(file) as full_text:
+	numbers = re.findall('[0-9]+',full_text.read())
+
+print(numbers)
+sum = 0
+for item in numbers:
+	sum += int(item)
+
+print(sum)
