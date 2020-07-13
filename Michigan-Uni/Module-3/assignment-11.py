3740c22235dfe943fab0d23d55e9ef3113ee69e4
Giulia Ciprandi <giulia.ciprandi@gmail.com>
2020-07-12 21:51:42 -0300

    Update assignment-11.py

--- Michigan-Uni/Module-3/assignment-11.py
+++ Michigan-Uni/Module-3/assignment-11.py
@@ -3,13 +3,16 @@
 
 import re
 
-file = "regex_sum_42.txt"
+file = "regex_sum_781438.txt"
 
 with open(file) as full_text:
 	numbers = re.findall('[0-9]+',full_text.read())
 
-sum = 0
+sum_ = 0
 for item in numbers:
-	sum += int(item)
+	sum_ += int(item)
 
-print(sum)
+print(sum_)
+
+##LIST COMPREHENSION
+print(sum( [int(n) for n in re.findall('[0-9]+',open(file).read())] ))
