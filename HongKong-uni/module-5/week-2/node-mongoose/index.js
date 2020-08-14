109342eeb05c71500e110ccb32acd8ca55e25064
Giulia Ciprandi <giulia.ciprandi@gmail.com>
2020-08-14 17:19:06 -0300

    Mongoose schema using mongoose commands

--- HongKong-uni/module-5/week-2/node-mongoose/index.js
+++ HongKong-uni/module-5/week-2/node-mongoose/index.js
@@ -17,29 +17,28 @@ connect.then((db) => {
 
     console.log('Connected correctly to server\n');
 
-    var newDish = Dishes({
-        name: 'Uthappizza',
-        description: 'test'
+    Dishes.create({
+        name: 'Uthapizza',
+        description: 'Test'
+    })
+    .then((dish) => {
+        console.log(dish + '\n');
+        
+        return Dishes.find({}).exec();
+    })
+    .then((dishes) => {
+        console.log(dishes + '\n');
+
+        return Dishes.deleteMany({});
+    })
+    .then(() => {
+        console.log('Operations performed succesfully.\n'+
+        'Closing connection to mongo.');
+
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
     });
-
-    newDish.save()
-        .then((dish) => {
-            console.log(dish + '\n');
-
-            return Dishes.find({});
-        })
-        .then((dishes) => {
-            console.log(dishes + '\n');
-
-            return Dishes.deleteMany({});
-        })
-        .then(() => {
-            console.log('Dishes were succefully deleted\n');
-
-            return mongoose.connection.close();
-        })
-        .catch((err) => {
-            console.log(err);
-        });
 
 });
