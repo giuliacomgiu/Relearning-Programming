b10469d97f026bb86a31df3a21658fee19b8f90c
Giulia Ciprandi <giulia.ciprandi@gmail.com>
2020-08-27 17:08:45 -0300

    Updated login environment to include more information on why things succeeded or failed

--- HongKong-uni/module-5/week-4/express-gen/conFusionServer/routes/users.js
+++ HongKong-uni/module-5/week-4/express-gen/conFusionServer/routes/users.js
@@ -10,7 +10,7 @@ router.use(bodyParser.json());
 
 // GET users listing. 
 router
-.options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
+.options('*', cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
 .get('/', cors.corsWithOptions, auth.verifyUser, auth.verifyAdmin, 
   function(req, res, next) 
 {
@@ -60,18 +60,49 @@ router
 });
 
 router
-.options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
-.post('/login', cors.corsWithOptions, passport.authenticate('local'), 
-  (req, res) => 
+.post('/login', cors.corsWithOptions, (req, res, next) => 
 {
+  passport.authenticate('local', (err, user, info) => {
+    //Info has details on why login failed
+    if (err) { return next(err) }
 
-  var token = auth.getToken({_id: req.user._id});
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.json({success: true, token: token, status: 'You are successfully logged in!'});
+    //Wrong input info
+    if (!user) {
+      res.statusCode = 401;
+      res.contentType('application/json');
+      return res.json({success: false, status: 'JWT invalid', err: info});
+    }
+
+    //Actual error
+    req.logIn(user, (err) => {
+      if(err) {
+        res.statusCode = 401;
+        res.contentType('application/json');
+        res.json({success: false, status: 'Login Unsuccessful!', 
+          err: 'Could not login user'});
+          return;
+      }
+
+      var token = auth.getToken({_id: req.user._id});
+      res.statusCode = 200;
+      res.contentType('application/json');
+      res.json({success: true, status: 'Login Successful!', token: token});
+    });
+  })(req, res, next);
 });
 
-module.exports = router;
+router.get('/logout', cors.corsWithOptions, (req, res, next) => {
+  if (req.session){
+    req.session.destroy();
+    res.clearCookie('session-id');
+    res.redirect('/');
+  } 
+  else {
+    var err = new Error('You are not logged in');
+    err.statusCode = 403;
+    next(err);
+  }
+})
 
 router.get('/facebook/token', passport.authenticate('facebook-token'), (req, res) => {
   if (req.user) {
@@ -81,3 +112,22 @@ router.get('/facebook/token', passport.authenticate('facebook-token'), (req, res
     res.json({success: true, token: token, status: 'You are successfully logged in!'});
   }
 });
+
+//Used to make sure token is still valid
+router.get('/checkJWTToken', cors.corsWithOptions, (req, res) => {
+  passport.authenticate('jwt', {session: false}, (err, user, info) => {
+    if (err) return next(err);
+    if(!user){
+      res.statusCode = 401;
+      res.contentType('application/json');
+      return res.json({status: 'JWT invalid!', success: false, err: info});
+    } 
+    else {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      return res.json({status: 'JWT valid!', success: true, user: user});
+    }
+  }) (req, res);
+});
+
+module.exports = router;
